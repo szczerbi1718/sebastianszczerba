ad2e4e0c9073bdb9419149a36e0aba7dd80e0399
SZCZERBI <sebastian.szczerbaa@gmail.com>
2020-11-29 21:54:44 +0100

    bootstrap

--- projekt/settings.py
+++ projekt/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -108,6 +110,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 
 
